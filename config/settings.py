729b0ba4725694266fba834266e2fb958a90a4a7
AMUL SHARMA <140779732+CODE-WITH-AMUL@users.noreply.github.com>
2026-01-03 13:41:06 +0545

    f(Config): Sucessfully config code structure

---
+++ config/settings.py
@@ -0,0 +1,12 @@
+# ----------------------------
+# Hotel Management System Settings
+# ----------------------------
+
+
+#----------[HOTEL NAME AND INFO]-----------#
+HOTEL_NAME = "OYO Hotel"
+CONTACT_INFO = 9765223785
+ADDRESS = "Kathmandu Nepal"
+EMAIL = "oyo@gamil.com"
+
+#----------[END OF HOTAL INFO]-----------#
